f43ce803563bbc9bae78bfb5d2369606b91d615a
Ridho Taufikurahman <ridhotaufik2003@gmail.com>
2023-11-25 23:44:53 +0800

    Membuat halaman tentang.js dengan fitur deskripsi tentang kami dan jumbotron

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,13 +1,13 @@
 //import AgendaKegiatan from '../views/pages/agenda-kegiatan';
 //import RiwayatAgenda from '../views/pages/riwayat-agenda';
-//import Tentang from '../views/pages/tentang';
+import Tentang from '../views/pages/tentang';
 import Beranda from '../views/pages/beranda';
 
 const routes = {
   '/': Beranda,
   //'/agenda-kegiatan': AgendaKegiatan,
   //'/riwayat-agenda': RiwayatAgenda,
-  //'/tentang': Tentang,
+  '/tentang': Tentang,
 };
 
 export default routes;
---
+++ src/scripts/views/pages/tentang.js
@@ -0,0 +1,49 @@
+// import { createRestaurantItemTemplate } from '../templates/templates-creator';
+
+const Tentang = {
+    async renderPageContent() {
+      return `
+      <div class="container">
+      <div class="hero_title">
+        <h1>Yuk Kenalan dengan AgenDes</h1>
+      </div>
+      <div class="hero_subtitle">
+        <p> Selamat datang di laman kami! Di sini, kami ingin berbagi dengan Anda mengenai visi misi kami, tentang kami sebagai tim, serta memberikan informasi tentang aplikasi yang kami hasilkan. Kami percaya bahwa dengan memahami lebih dalam mengenai nilai-nilai kami dan tujuan kami dalam menciptakan solusi yang bermanfaat, Anda akan semakin yakin dengan apa yang kami tawarkan.
+        </p>
+      </div>
+      <a
+        class="exchange_button" href="#/agenda">
+        Cek Agenda
+      </a>
+      <img class="image_hero" src="beranda_riwayat.png" alt="Hero" />
+    </div>
+    
+    
+    <div class="maincontent" id="maincontent" >
+      <div class="deskripsi_container">
+        <div class="tentangkami">
+          <h1 class="teks-tentangkami">Tentang Kami</h1>
+        </div>
+        <div class="deskripsi_paragraf">
+          <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+          officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+      </div>
+      
+    </div>
+    `;
+    },
+  
+    async applyDataContent() {
+      // TODO: Isi data halaman
+    },
+  };
+  
+  export default Tentang;
+  
