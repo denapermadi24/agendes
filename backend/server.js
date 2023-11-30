a7dc0ba8869549d4db1e03a22ee66b8a5f013163
AmeliaDevasya <amel.dvsy@gmail.com>
2023-11-30 13:02:37 +0800

    upload sebuah gambar

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const multer = require('multer');
 
 //parse application/json
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// Membuat storage engine untuk menyimpan file di folder 'uploads'
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(upload.single('foto_kegiatan'));
 
 //panggil routes
 var routes = require('./routes');
@@ -13,3 +29,5 @@ routes(app);
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
+
+app.use('/uploads', express.static('uploads'));
