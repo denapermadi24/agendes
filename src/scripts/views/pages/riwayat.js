8f3f1000b0734398b8b25a1a1045f0ef5e516f1b
Jalalln4497 <jalalln168@gmail.com>
2023-12-04 00:43:51 +0800

    Halaman Riwayat Agenda selesai dibuat dan memperbaiki beberapa tampilan di beberapa halaman

--- src/scripts/views/pages/riwayat.js
+++ src/scripts/views/pages/riwayat.js
@@ -4,33 +4,143 @@ const RiwayatAgenda = {
   async renderPageContent() {
     return `
     <div class="container-riwayat-agenda shadow">
-      <img class="img-riwayat-kegiatan" src="./riwayat_agenda.png" alt="Hero" width="450" />
-      <div class="desc_hero">
-        <div class="hero_title">
-          <h1>Temukan histori agenda kegiatan desa disini!</h1>
-        </div>
-        <div class="hero_subtitle">
-          <p>AgenDes dilengkapi dengan fitur pencarian histori dan detail kegiatan yang memudahkan Anda dalam pencatatan
-            riwayat dan evaluasi untuk kegiatan selanjutnya.
-          </p>
-          <input type="search" class="search-input" placeholder="Nama / tanggal kegiatan">
-          <button class="btn-search">Cari</button>
-        </div>
+    <img class="img-riwayat-kegiatan" src="./riwayat_agenda.png" alt="Hero" width="450" />
+    <div class="desc_hero">
+      <div class="hero_title">
+        <h1>Temukan histori agenda kegiatan desa disini!</h1>
+      </div>
+      <div class="hero_subtitle">
+        <p>AgenDes dilengkapi dengan fitur pencarian histori dan detail kegiatan yang memudahkan Anda dalam pencatatan
+          riwayat dan evaluasi untuk kegiatan selanjutnya.
+        </p>
+        <input type="search" class="search-input" placeholder="Nama / tanggal kegiatan">
+        <button class="btn-search">Cari</button>
       </div>
     </div>
+  </div>
 
-    <div class="main-content-container">
-      <div class="container-post-kegiatan">
-        <div class="post-kegiatan">
-          <div class="form-post-kegiatan">
-            <section class="input_section shadow">
-              <h2>Riwayat Kegiatan</h2>
-
+  <div class="main-content-container">
+    <section class="riwayat-kegiatan">
+      <h2>Riwayat Kegiatan</h2>
+      <hr>
+      <section class="list-riwayat-kegiatan">
+        <article class="item-riwayat-kegiatan shadow">
+          <img src="./jadwal.png" class="list-img-riwayat-kegiatan" alt="gambar list riwayat">
+          <section class="item-riwayat-article">
+            <a href="#">
+              <h3>Gotong Royong Membersihkan Pantai</h3>
+            </a>
+            <p>Tidak hanya menikmati keindahan pantai, tetapi juga merawatnya. Ayo bergabung dalam upaya
+              bersih-bersih pantai untuk mewujudkan pantai yang lestari dan indah.</p>
+            <section class="waktu-kegiatan">
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-calendar icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                </svg>
+                16 November 2023
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-clock icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                </svg>
+                08.00 - 11.00 WITA
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-geo-alt icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+                Pantai Sanur
+              </p>
             </section>
-          </div>
-        </div>
-      </div>
-    </div>
+          </section>
+        </article>
+
+        <article class="item-riwayat-kegiatan shadow">
+          <img src="./jadwal.png" class="list-img-riwayat-kegiatan" alt="gambar list riwayat">
+          <section class="item-riwayat-article">
+            <a href="#">
+              <h3>Gotong Royong Membersihkan Pantai</h3>
+            </a>
+            <p>Tidak hanya menikmati keindahan pantai, tetapi juga merawatnya. Ayo bergabung dalam upaya
+              bersih-bersih pantai untuk mewujudkan pantai yang lestari dan indah.</p>
+            <section class="waktu-kegiatan">
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-calendar icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                </svg>
+                16 November 2023
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-clock icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                </svg>
+                08.00 - 11.00 WITA
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-geo-alt icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+                Pantai Sanur
+              </p>
+            </section>
+          </section>
+        </article>
+
+        <article class="item-riwayat-kegiatan shadow">
+          <img src="./jadwal.png" class="list-img-riwayat-kegiatan" alt="gambar list riwayat">
+          <section class="item-riwayat-article">
+            <a href="#">
+              <h3>Gotong Royong Membersihkan Pantai</h3>
+            </a>
+            <p>Tidak hanya menikmati keindahan pantai, tetapi juga merawatnya. Ayo bergabung dalam upaya
+              bersih-bersih pantai untuk mewujudkan pantai yang lestari dan indah.</p>
+            <section class="waktu-kegiatan">
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-calendar icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                </svg>
+                16 November 2023
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-clock icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                </svg>
+                08.00 - 11.00 WITA
+              </p>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi-geo-alt icon-waktu-kegiatan" viewBox="0 0 16 16">
+                  <path
+                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+                Pantai Sanur
+              </p>
+            </section>
+          </section>
+        </article>
+
+      </section>
+    </section>
+  </div>
   `;
   },
 
