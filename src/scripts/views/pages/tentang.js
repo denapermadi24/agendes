ee732a3c5db4738fc6988ce3abc22b2efa6c6685
Ridho Taufikurahman <ridhotaufik2003@gmail.com>
2023-12-15 01:21:59 +0800

    Menambahkan link linkedin muhammad febrian haekal pada halaman tentang

--- src/scripts/views/pages/tentang.js
+++ src/scripts/views/pages/tentang.js
@@ -88,7 +88,7 @@ const Tentang = {
           </div>
           <div class="icon_medsos-avtr">
             <a href="https://www.instagram.com/febrianhaekal1/" class="medsos" target="_blank"><i class="fa fa-instagram"></i></a>
-            <a href="#" class="medsos"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/mfebrian-haekal/" class="medsos" target="_blank"><i class="fa fa-linkedin"></i></a>
           </div>
         </div>
         <div class="nama_team">
