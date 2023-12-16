f3885f940ef1f0fccd9e6531176a70809d1d5cb7
Ridho Taufikurahman <ridhotaufik2003@gmail.com>
2023-12-16 23:42:04 +0800

    menampilkan foto dena pada halaman tentang

--- src/scripts/views/pages/tentang.js
+++ src/scripts/views/pages/tentang.js
@@ -120,7 +120,7 @@ const Tentang = {
       <div class="team_card">
         <div class="team_avatar">
           <div class="team_image">
-            <img class="icon_team" src="test.png" alt="Team Icon" />
+            <img class="icon_team" src="dena.png" alt="Team Icon" />
           </div>
           <div class="icon_medsos-avtr">
             <a href="https://www.instagram.com/dnaprmdi/" class="medsos" target="_blank"><i class="fa fa-instagram"></i></a>
