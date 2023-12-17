1785216474bcde49ad72eb042c5dff96eaefb41e
febrayen <febrayen11@gmail.com>
2023-12-17 15:07:42 +0800

    menambah foto jalal

--- src/scripts/views/pages/tentang.js
+++ src/scripts/views/pages/tentang.js
@@ -48,7 +48,7 @@ const Tentang = {
       <div class="team_card">
         <div class="team_avatar">
           <div class="team_image">
-            <img class="icon_team" src="test.png" alt="Team Icon" />
+            <img class="icon_team" src="jalal.png" alt="Team Icon" />
           </div>
           <div class="icon_medsos-avtr">
             <a href="https://www.instagram.com/jaalalln/" class="medsos" target="_blank"><i class="fa fa-instagram"></i></a>
